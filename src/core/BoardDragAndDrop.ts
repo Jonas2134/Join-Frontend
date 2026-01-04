7671c5490f0e42237cd939495e9eb1600e1e1c16
Jonas Stiefer <neo.stief.jon@gmail.com>
2026-01-04 18:17:39 +0100

    finish Board Drag and Drop

--- src/core/BoardDragAndDrop.ts
+++ src/core/BoardDragAndDrop.ts
@@ -29,74 +29,52 @@ export class BoardDragAndDrop {
     const droppables = root.querySelectorAll<HTMLElement>(".task-list");
 
     draggables.forEach((task) => {
-      task.addEventListener("dragstart", () => {
-        const column = task.closest<HTMLElement>(".board-column");
-        const list = task.closest<HTMLElement>(".task-list");
-
-        if (!column || !list) return;
-
-        this.dragged = {
-          taskId: Number(task.dataset.taskId),
-          taskPosition: Array.from(list.querySelectorAll<HTMLElement>(".task")).indexOf(task) + 1,
-          columnId: Number(column.dataset.columnId),
-        };
-        this.droped = null;
-        task.classList.add("is-dragging");
-      });
-
-      task.addEventListener("dragend", () => {
-        task.classList.remove("is-dragging");
-      });
+      task.addEventListener("dragstart", this.onTaskDragstart.bind(this, task));
+      task.addEventListener("dragend", this.onTaskDragend.bind(this, task));
     });
 
     droppables.forEach((zone) => {
-      zone.addEventListener("dragover", (e) => {
-        e.preventDefault();
-
-        const curTask = root.querySelector<HTMLElement>(".is-dragging");
-        if (!curTask) return;
-
-        const bottomTask = this.insertAboveTask(zone, e.clientY);
-        if (!bottomTask) {
-          if (zone.lastElementChild !== curTask) {
-            zone.appendChild(curTask);
-          }
-        } else if (bottomTask.previousElementSibling !== curTask) {
-          zone.insertBefore(curTask, bottomTask);
-        }
-
-        const column = zone.closest<HTMLElement>(".board-column");
-        if (!column) return;
-        const tasks = Array.from(zone.querySelectorAll<HTMLElement>(".task"));
-
-        this.droped = {
-          targetColumnId: Number(column.dataset.columnId),
-          targetPosition: tasks.indexOf(curTask) + 1
-        };
-      });
-
-      zone.addEventListener("drop", async () => {
-        if (!this.dragged || !this.droped) return;
-
-        const sameColumn = this.dragged.columnId === this.droped.targetColumnId;
-        const samePosition = this.dragged.taskPosition === this.droped.targetPosition;
-
-        if (sameColumn && samePosition) {
-          this.dragged = null;
-          this.droped = null;
-          return;
-        }
-
-        await appStore.updateTask(String(this.dragged.taskId), {
-          column: this.droped.targetColumnId,
-          position: this.droped.targetPosition
-        });
-
-        this.dragged = null;
-        this.droped = null;
-        this.onUpdate();
-      });
+      zone.addEventListener("dragover", e => this.onTaskDragover(e, zone, root));
     });
+  }
+
+  private onTaskDragstart(task: HTMLElement): void {
+    const column = task.closest<HTMLElement>(".board-column");
+    const list = task.closest<HTMLElement>(".task-list");
+
+    if (!column || !list) return;
+
+    this.dragged = {
+      taskId: Number(task.dataset.taskId),
+      taskPosition: Array.from(list.querySelectorAll<HTMLElement>(".task")).indexOf(task) + 1,
+      columnId: Number(column.dataset.columnId),
+    };
+    this.droped = null;
+    task.classList.add("is-dragging");
+  }
+
+  private async onTaskDragend(task: HTMLElement): Promise<void> {
+    task.classList.remove("is-dragging");
+
+    if (!this.dragged || !this.droped) return;
+
+    const sameColumn = this.dragged.columnId === this.droped.targetColumnId;
+    const samePosition = this.dragged.taskPosition === this.droped.targetPosition;
+
+    if (sameColumn && samePosition) {
+      this.dragged = null;
+      this.droped = null;
+      return;
+    }
+
+    await appStore.updateTask(String(this.dragged.taskId), {
+      column: this.droped.targetColumnId,
+      position: this.droped.targetPosition,
+    });
+
+    this.dragged = null;
+    this.droped = null;
+    this.onUpdate();
   }
 
   private insertAboveTask(
@@ -118,5 +96,34 @@ export class BoardDragAndDrop {
       }
     });
     return closestTask;
+  }
+
+  private onTaskDragover(
+    e: DragEvent,
+    zone: HTMLElement,
+    root: HTMLElement
+  ): void {
+    e.preventDefault();
+
+    const curTask = root.querySelector<HTMLElement>(".is-dragging");
+    if (!curTask) return;
+
+    const bottomTask = this.insertAboveTask(zone, e.clientY);
+    if (!bottomTask) {
+      if (zone.lastElementChild !== curTask) {
+        zone.appendChild(curTask);
+      }
+    } else if (bottomTask.previousElementSibling !== curTask) {
+      zone.insertBefore(curTask, bottomTask);
+    }
+
+    const column = zone.closest<HTMLElement>(".board-column");
+    if (!column) return;
+    const tasks = Array.from(zone.querySelectorAll<HTMLElement>(".task"));
+
+    this.droped = {
+      targetColumnId: Number(column.dataset.columnId),
+      targetPosition: tasks.indexOf(curTask) + 1,
+    };
   }
 }
