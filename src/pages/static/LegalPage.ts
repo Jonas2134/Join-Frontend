d14c4f54160ca4da6bc3b7b8eacf5d9c77b2288f
Jonas Stiefer <neo.stief.jon@gmail.com>
2026-03-20 16:40:36 +0100

    add Legal Notice

--- src/pages/static/LegalPage.ts
+++ src/pages/static/LegalPage.ts
@@ -1,8 +1,41 @@
-import { BaseStaticPage } from './BaseStaticPage';
+import { BaseStaticPage } from "./BaseStaticPage";
 
 export class LegalPage extends BaseStaticPage {
-  protected headline = 'Legal Notice';
+  protected headline = "Legal Notice";
   protected content = /*html*/ `
-    <p><!-- Impressum hier einfügen --></p>
+    <strong>Angaben gemäß § 5 DDG</strong>
+    <p>
+      Jonas Stiefer
+      <br> 
+      Fissgässen 16
+      <br> 
+      L-9940 Asselborn
+      <br>
+      <br>
+    </p>
+    <p>
+      <strong>Vertreten durch: </strong>
+      <br>
+      Jonas Stiefer
+      <br>
+      <br>
+    </p>
+    <p>
+      <strong>Kontakt:</strong>
+      <br>
+      E-Mail: <a href="mailto:connect@jonas-stiefer.com">connect@jonas-stiefer.com</a>
+      <br>
+      <br>
+    </p>
+    <p>
+      <strong>Verbraucherstreitbeilegung / Universalschlichtungsstelle</strong>
+      <br>
+      Wir nehmen nicht an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teil und sind dazu auch nicht verpflichtet.
+      <br>
+      <br>
+    </p>
+    <p>
+      Erstellt mit dem <a href="https://impressum-generator.de" rel="dofollow">Impressum-Generator</a> von WebsiteWissen.com, dem Ratgeber für <a href="https://websitewissen.com/website-erstellen" rel="dofollow">Website-Erstellung</a>, <a href="https://websitewissen.com/homepage-baukasten-vergleich" rel="dofollow">Homepage-Baukästen</a> und <a href="https://websitewissen.com/shopsysteme-vergleich" rel="dofollow">Shopsysteme</a>. Rechtstext von der <a href="https://www.kanzlei-hasselbach.de/" rel="dofollow">Kanzlei Hasselbach</a>. 
+    </p>
   `;
 }
